862f2d9b3ef3e0bf7a6f29dcdf3e4374465043c8
v002 <v002@benner.com.br>
2018-07-12 15:14:17 -0300

    Condutores - nome cliente

---
+++ T01/Construtores/Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtores
+{
+
+    class Conta
+    {
+
+        public int numero { get; set; }
+
+        public double Saldo { get; private set; }
+
+        public Cliente Titular { get; set; }
+
+        public void Saca(double valor)
+        {
+            this.Saldo -= valor + 0.1;
+        }
+
+        public void Deposita(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public void Transfere(double valor, Conta destino)
+        {
+            this.Saca(valor);
+            destino.Deposita(valor);
+        }
+    }
+}
