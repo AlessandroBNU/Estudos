05057418685aa02da3372589ad31e3c8fde074d8
v002 <v002@benner.com.br>
2018-07-12 15:52:11 -0300

    Exibindo o nome para o usuário

---
+++ T01/CaixaEletronico/Conta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+
+    class Conta
+    {
+        public int Numero { get; set; }
+        public Cliente Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public void Deposita(double valorASerDepositado)
+        {
+            if (valorASerDepositado > 0)
+            {
+                this.Saldo += valorASerDepositado;
+            }
+        }
+        
+    }
+}
