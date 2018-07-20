595f2c9147994533b6d5c827690eda9ec49f0d07
v002 <v002@benner.com.br>
2018-07-20 16:27:59 -0300

    Criação de Classes e Interface.

--- T01/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T01/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -26,34 +26,32 @@ namespace Alura.Loja.Testes.ConsoleApp
             RecuperarProdutos();
 
             //atualiza o produto
-            using (var repo = new LojaContext())
+            using (var repo = new ProdutoDAOEntity())
             {
-               Produto primeiro = repo.Produtos.First();
+               Produto primeiro = repo.Produtos().First();
                 primeiro.Nome = "Cassino Royale - Editado";
-                repo.Produtos.Update(primeiro);
-                repo.SaveChanges();
+                repo.Atualizar(primeiro);
             }
             RecuperarProdutos();
         }
 
         private static void ExcluirProdutos()
         {
-            using ( var repo = new LojaContext())
+            using ( var repo = new ProdutoDAOEntity())
             {
-                IList<Produto> produtos = repo.Produtos.ToList();
+                IList<Produto> produtos = repo.Produtos();
                 foreach (var item in produtos)
                 {
-                    repo.Produtos.Remove(item);
+                    repo.Remover(item);
                 }
-                repo.SaveChanges();
             }
         }
 
         private static void RecuperarProdutos()
         {
-            using (var repo = new LojaContext())
+            using (var repo = new ProdutoDAOEntity())
             {
-                IList<Produto> produtos = repo.Produtos.ToList();
+                IList<Produto> produtos = repo.Produtos();
                 Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
                foreach (var item in produtos)
                 {
@@ -68,34 +66,12 @@ namespace Alura.Loja.Testes.ConsoleApp
             p.Nome = "Harry Potter e a Ordem da Fênix";
             p.Categoria = "Livros";
             p.Preco = 19.89;
-
-            Produto p1 = new Produto();
-            p1.Nome = "Harry Potter e a Ordem da Fênix";
-            p1.Categoria = "Livros";
-            p1.Preco = 19.89;
-
-            Produto p2 = new Produto();
-            p2.Nome = "Harry Potter e a Ordem da Fênix";
-            p2.Categoria = "Livros";
-            p2.Preco = 19.89;
-            using (var contexto = new LojaContext())
-            {
-                contexto.Produtos.AddRange(p, p1, p2);
-                contexto.SaveChanges();
-            }
-        }
-
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAO())
+            using (var repo = new ProdutoDAOEntity())
             {
                 repo.Adicionar(p);
+                
             }
         }
+
     }
 }
