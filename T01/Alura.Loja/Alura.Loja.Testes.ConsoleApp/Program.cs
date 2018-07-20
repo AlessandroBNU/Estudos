7bc66745899f1f549596a254345f08a1bd728061
v002 <v002@benner.com.br>
2018-07-20 16:31:32 -0300

    Novo projeto de teste

--- T01/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T01/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,68 +10,8 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //GravarUsandoAdoNet();
-            //GravarUsandoEntity();
-            //RecuperarProdutos();
-            //ExcluirProdutos();
-            //RecuperarProdutos();
-            AtualizarProtudo();
-        }
-
-        private static void AtualizarProtudo()
-        {
-            //incluir um produto
-
-            GravarUsandoEntity();
-            RecuperarProdutos();
-
-            //atualiza o produto
-            using (var repo = new ProdutoDAOEntity())
-            {
-               Produto primeiro = repo.Produtos().First();
-                primeiro.Nome = "Cassino Royale - Editado";
-                repo.Atualizar(primeiro);
-            }
-            RecuperarProdutos();
-        }
-
-        private static void ExcluirProdutos()
-        {
-            using ( var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                foreach (var item in produtos)
-                {
-                    repo.Remover(item);
-                }
-            }
-        }
-
-        private static void RecuperarProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
-               foreach (var item in produtos)
-                {
-                    Console.WriteLine(item.Nome);
-                }
-            }
-        }
-
-        private static void GravarUsandoEntity()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-            using (var repo = new ProdutoDAOEntity())
-            {
-                repo.Adicionar(p);
-                
-            }
-        }
+           
+        } 
 
     }
 }
