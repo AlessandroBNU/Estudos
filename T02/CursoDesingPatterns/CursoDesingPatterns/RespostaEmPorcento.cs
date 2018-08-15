60c22a9ae3e4ef449cf60f6193f782185e4f4717
v002 <v002@benner.com.br>
2018-08-15 15:46:37 -0300

    Recebendo o próximo ítem da corrente pelo construtor.

--- T02/CursoDesingPatterns/CursoDesingPatterns/RespostaEmPorcento.cs
+++ T02/CursoDesingPatterns/CursoDesingPatterns/RespostaEmPorcento.cs
@@ -10,15 +10,32 @@ namespace CursoDesingPatterns
     {
         public IResposta OutraResposta { get; set; }
 
+
+        public RespostaEmPorcento(IResposta outraResposta)
+        {
+            this.OutraResposta = outraResposta;
+        }
+
+        public RespostaEmPorcento()
+        {
+            this.OutraResposta = null; //nao recebi a proxima!
+        }
+
         public void IResponde(Requisicao req, Conta conta)
         {
             if(req.Formato == Formato.PORCENTO)
             {
                 Console.WriteLine(conta.Titular + "%" + conta.Saldo);
             }
-            else
+            else if(OutraResposta != null)
             {
                 OutraResposta.IResponde(req, conta);
+            }
+            else
+            {
+                //não existe próxima na corrente, e ninguém atendeu a requisição !
+                //poderíamos não ter feito nada aqui, caso não fosse necessário!
+                throw new Exception("Formato de resposta não encontrado");
             }
         }
     }
