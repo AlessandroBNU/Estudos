09449af2ce57e46be3c1f8c1d5ccdf5dae29ce3f
Alessandro <v002@benner.com.br>
2018-08-16 16:53:10 -0300

    CCriação de impostos muito alto com Decorator

---
+++ T02/CursoDesingPatterns/CursoDesingPatterns/TesteImposto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesingPatterns
+{
+   public class TesteImposto
+    {
+        static void Main(String[] args)
+        {
+            Imposto imposto = new ISS(new ICMS(new ImpostoMuitoAlto()));
+            Orcamento orcamento = new Orcamento(500.0);
+            Console.WriteLine(imposto.Calcula(orcamento));
+
+            
+            
+        }
+    }
+}
